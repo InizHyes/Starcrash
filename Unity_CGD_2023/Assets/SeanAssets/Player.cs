1db2d173c9540be4c272d042065ded3cd7ec8a5e
GitExcel <sean.harding@live.co.uk>
2023-10-24 00:32:41 +0100

    Controller Support
    Can control one character with keyboard and mouse and one with controller. Cannot figure out how to do two controllers

--- Unity_CGD_2023/Assets/SeanAssets/Player.cs
+++ Unity_CGD_2023/Assets/SeanAssets/Player.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 { /// some variables below may be uselss, was testing many things
@@ -16,44 +18,95 @@ public class PlayerController : MonoBehaviour
     Vector2 ForceDir;
     Vector2 LastVel;
     public float SpeedCap = 10;
+    public int player = 0;
+    [SerializeField] private InputActionReference movement, attack, rotate;
+    private bool shoot = false;
+    [SerializeField] PlayerInput playerinput;
 
-    // Start is called before the first frame update
-    void Start()
+
+
+   
+    
+    private void OnEnable() ///handles the inputs, buttons only get detected like this
     {
+        attack.action.performed += AttackPressed;
+        print("yes");
+        
+    }
+    private void OnDisable() ///disables the function when the button is released
+    {
+        attack.action.performed -= AttackPressed;
+    }
 
+    private void AttackPressed(InputAction.CallbackContext context) ///makes shoot true which makes the chcrater shoot in update
+    {
+        shoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized; ///gets playerinput
-        Vector2 MoveForce = PlayerInput * MoveSpeed; ///applies the speed to player input
-        MoveForce2 = MoveForce2 + PlayerInput * MoveSpeed; ///adding the players input onto the current move vector 2, never loses momentum as it is adding rather than setting
-        MoveForce2 += ForceToApply; ///force to apply is instant force, e.g if you wanted to fire a gun (below in mouse section) change apply force to alter current movement
-        ForceToApply /= ForceDamping; ///so you arent just constantly adding the same force
-        rb.velocity = MoveForce2; ///actually where movment happens
-
-
-
-        Vector2 MouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;  ///these three lines make the player look at the mouse
-        var angle = Mathf.Atan2(MouseDir.y, MouseDir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-
-
-
-        if (Input.GetMouseButtonDown(0)) ///this function checks where the mouse is clicked and applies force to the player in the opposite direction
+        
+        rb.velocity = MoveForce2; ///actually where movment happen
+        if (player == 1) ///KEYBOARD CONTROLLS
         {
-            print("GunFired");
-            MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            ///print(MousePos);
-            PlayerPos = transform.position;
-            ///print(PlayerPos);
-            ForceDir = (MousePos - PlayerPos).normalized;
-            ///print(ForceDir);
-            ForceToApply = (ForceDir * GunForce * -1.0f); ///change gunforce to change knockback effect
-                                                          ///rb.AddForce(ForceDir * GunForce * -1.0f);
+            Vector2 PlayerInput = new Vector2(Input.GetAxisRaw("HorizontalKeyboard"), Input.GetAxisRaw("VerticalKeyboard")).normalized; ///gets playerinput
+            Vector2 MoveForce = PlayerInput * MoveSpeed; ///applies the speed to player input
+            MoveForce2 = MoveForce2 + PlayerInput * MoveSpeed; ///adding the players input onto the current move vector 2, never loses momentum as it is adding rather than setting
+            MoveForce2 += ForceToApply; ///force to apply is instant force, e.g if you wanted to fire a gun (below in mouse section) change apply force to alter current movement
+            ForceToApply /= ForceDamping; ///so you arent just constantly adding the same force
+            rb.velocity = MoveForce2; ///actually where movment happen
+            Vector2 MouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;  ///these three lines make the player look at the mouse
+            var angle = Mathf.Atan2(MouseDir.y, MouseDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+
+
+            if (Input.GetMouseButtonDown(0)) ///this function checks where the mouse is clicked and applies force to the player in the opposite direction
+            {
+                print("GunFired");
+                MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                ///print(MousePos);
+                PlayerPos = transform.position;
+                ///print(PlayerPos);
+                ForceDir = (MousePos - PlayerPos).normalized;
+                ///print(ForceDir);
+                ForceToApply = (ForceDir * GunForce * -1.0f); ///change gunforce to change knockback effect
+                                                              ///rb.AddForce(ForceDir * GunForce * -1.0f);
+            }
+
         }
+        else if (player == 2) ///CONTROLLER CONTROLS
+        {
+           
+            Vector2 PlayerInput = movement.action.ReadValue<Vector2>();
+            Vector2 MoveForce = PlayerInput * MoveSpeed; ///applies the speed to player input
+            MoveForce2 = MoveForce2 + PlayerInput * MoveSpeed; ///adding the players input onto the current move vector 2, never loses momentum as it is adding rather than setting
+            MoveForce2 += ForceToApply; ///force to apply is instant force, e.g if you wanted to fire a gun (below in mouse section) change apply force to alter current movement
+            ForceToApply /= ForceDamping; ///so you arent just constantly adding the same force
+            rb.velocity = MoveForce2; ///actually where movment happen
+            Vector2 RightStick = rotate.action.ReadValue<Vector2>();
+            var angle = Mathf.Atan2(RightStick.y, RightStick.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            if (shoot == true)  ///this function checks if shoot is true, then "shoots"
+            {
+                print("GunFired");
+                MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                ForceDir = transform.right;
+                ForceToApply = (ForceDir * GunForce * -1.0f); ///change gunforce to change knockback effect
+                shoot = false;
+                                                              
+                    
+            }
+
+
+
+
+
+        }
+
 
 
 
