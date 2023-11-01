7a862c890d391e3942882ea5f8075a7dced9631d
masonwant <masonwant@gmail.com>
2023-11-01 04:16:33 +0000

    Level Generator v1.1
    + Added comments throughout code
    + Added a room repetition toggle

--- Unity_CGD_2023/Assets/Scripts/Level Scripts/LevelGenerator.cs
+++ Unity_CGD_2023/Assets/Scripts/Level Scripts/LevelGenerator.cs
@@ -4,36 +4,55 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-
     [SerializeField] private Transform StartingRoom;
-    [SerializeField] private List <Transform> roomList;
+    [SerializeField] private List<Transform> roomList;
 
     private Vector3 lastRoomExit;
     [SerializeField] private int roomsToSpawn;
 
-    private void Awake()
-    { 
+    // Create a list to keep track of spawned rooms.
+    private List<Transform> spawnedRooms = new List<Transform>();
+
+    [SerializeField] private bool allowRoomRepetition = false; // Toggle for room repetition.
+
+    private void Start()
+    {
         lastRoomExit = StartingRoom.Find("RoomExit").position;
-        
+
         for (int i = 0; i < roomsToSpawn; i++)
         {
-        RoomSpawner();
+            RoomSpawner();
         }
-
-
     }
 
     private void RoomSpawner()
     {
+        if (roomList.Count == 0)
+        {
+            Debug.LogError("No room prefabs assigned to roomList!");
+            return;
+        }
+
         Transform roomListSelect = roomList[Random.Range(0, roomList.Count)];
-        Transform lastRoomTransform = RoomSpawner(roomListSelect, lastRoomExit);
-        lastRoomExit = lastRoomTransform.Find("RoomExit").position;
+
+        // Check if the selected room has not already been spawned, or room repetition is allowed.
+        if (!allowRoomRepetition && spawnedRooms.Contains(roomListSelect))
+        {
+            Debug.Log("Selected room already spawned. Skipping.");
+        }
+        else
+        {
+            Transform lastRoomTransform = RoomSpawner(roomListSelect, lastRoomExit);
+            lastRoomExit = lastRoomTransform.Find("RoomExit").position;
+
+            // Add the spawned room to the list of spawned rooms.
+            spawnedRooms.Add(roomListSelect);
+        }
     }
 
-    private Transform RoomSpawner(Transform roomList, Vector3 spawnLocation)
+    private Transform RoomSpawner(Transform roomPrefab, Vector3 spawnLocation)
     {
-        Transform RoomTransform = Instantiate(roomList, spawnLocation, Quaternion.identity);
-
-        return RoomTransform;
+        Transform roomTransform = Instantiate(roomPrefab, spawnLocation, Quaternion.identity);
+        return roomTransform;
     }
 }
