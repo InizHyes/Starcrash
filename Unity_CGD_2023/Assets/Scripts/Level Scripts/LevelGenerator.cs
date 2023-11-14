588eca30cbf7da82b3e2c67db70ed63a18ee413c
masonwant <masonwant@gmail.com>
2023-11-14 14:42:38 +0000

    Level Generator v2.1
    + Added Boss Room Frequency
    + Added Room Repetition
    + Added ability to adjust room difficulty spawning order

--- Unity_CGD_2023/Assets/Scripts/Level Scripts/LevelGenerator.cs
+++ Unity_CGD_2023/Assets/Scripts/Level Scripts/LevelGenerator.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private Transform StartingRoom;
-    [SerializeField] private List<Transform> roomList;
+    [SerializeField] private List<Transform> roomList; // List of regular rooms
+    [SerializeField] private List<Transform> finalRoomList; // List of final rooms
     [SerializeField] private int roomsToSpawn;
-    [SerializeField] private bool allowRoomRepetition = false;
-    [SerializeField] private int minRoomsBeforeHard = 5; // Minimum number of rooms before harder levels start.
+    [SerializeField] private bool allowRoomRepetition = false; // Allow the same room to be spawned multiple times
+    [SerializeField] private int minRoomsBeforeHard = 5; // Minimum number of rooms before harder levels start
+    [SerializeField] private int bossRoomFrequency = 5; // Spawn a boss room every 'bossRoomFrequency' regular rooms
+    [SerializeField] private Transform bossRoomPrefab; // Assign the boss room prefab in the Inspector
+    [SerializeField] private int minRoomsBeforeBoss = 3; // Minimum number of regular rooms required before a boss room can spawn
 
     private Vector3 lastRoomExit;
-    private int playerProgress = 1; // Represents the player's progression level.
-
-    // A list to keep track of spawned rooms.
+    private int playerProgress = 1;
     private List<Transform> spawnedRooms = new List<Transform>();
     private bool hardRoomsUnlocked = false;
+    private int bossRoomCounter = 0;
 
     private void Start()
     {
         lastRoomExit = StartingRoom.Find("RoomExit").position;
+        int regularRoomsBeforeBoss = minRoomsBeforeBoss > 0 ? minRoomsBeforeBoss : 1;
 
         for (int i = 0; i < roomsToSpawn; i++)
         {
-            RoomSpawner();
+            if (i >= roomsToSpawn - 1)
+            {
+                // Spawn the final room at the end
+                SpawnFinalRoom();
+            }
+            else if (bossRoomCounter >= bossRoomFrequency && i >= regularRoomsBeforeBoss)
+            {
+                // Spawn a boss room based on the frequency
+                SpawnBossRoom();
+                bossRoomCounter = 0;
+            }
+            else
+            {
+                // Spawn a regular room
+                RoomSpawner();
+                bossRoomCounter++;
+            }
         }
     }
 
@@ -35,7 +55,6 @@ public class LevelGenerator : MonoBehaviour
             return;
         }
 
-        // Adjust the difficulty level based on player progression.
         int difficultyLevel = Mathf.Max(1, playerProgress - minRoomsBeforeHard);
 
         if (difficultyLevel >= 2)
@@ -43,20 +62,11 @@ public class LevelGenerator : MonoBehaviour
             hardRoomsUnlocked = true;
         }
 
-        // Select a room from the list based on the current difficulty level.
-        List<Transform> roomListForDifficulty = roomList;
-
-        if (hardRoomsUnlocked)
-        {
-            roomListForDifficulty = roomListHard;
-        }
-
-        // Select a room randomly based on room repetition settings.
-        Transform roomListSelect = SelectRandomRoom(roomListForDifficulty);
+        Transform roomListSelect = SelectRandomRoom(hardRoomsUnlocked ? roomListHard : roomList);
 
         if (roomListSelect == null)
         {
-            // Handle the case when no room can be selected (all rooms have been spawned).
+            // All rooms have been spawned
             Debug.Log("All rooms have been spawned.");
             return;
         }
@@ -67,8 +77,6 @@ public class LevelGenerator : MonoBehaviour
         {
             lastRoomExit = lastRoomTransform.Find("RoomExit").position;
             spawnedRooms.Add(roomListSelect);
-
-            // Increase the player's progression.
             playerProgress++;
         }
     }
@@ -77,7 +85,6 @@ public class LevelGenerator : MonoBehaviour
     {
         if (!allowRoomRepetition)
         {
-            // Create a list of unspawned rooms for the current difficulty level.
             List<Transform> unspawnedRooms = new List<Transform>();
 
             foreach (Transform room in roomListForDifficulty)
@@ -88,22 +95,22 @@ public class LevelGenerator : MonoBehaviour
                 }
             }
 
-            // Check if there are unspawned rooms left.
             if (unspawnedRooms.Count > 0)
             {
-                // Select a random unspawned room from the list.
+                // Select a random unspawned room from the list
                 int randomIndex = Random.Range(0, unspawnedRooms.Count);
                 return unspawnedRooms[randomIndex];
             }
             else
             {
-                // No unspawned rooms left for this difficulty level.
+                // No unspawned rooms left for this difficulty level
+                Debug.Log("No unspawned rooms left for this difficulty level.");
                 return null;
             }
         }
         else if (roomListForDifficulty.Count > 0)
         {
-            // Allow room repetition, so select a random room from the entire list.
+            // Allow room repetition, so select a random room from the entire list
             int randomIndex = Random.Range(0, roomListForDifficulty.Count);
             return roomListForDifficulty[randomIndex];
         }
@@ -113,10 +120,49 @@ public class LevelGenerator : MonoBehaviour
 
     private Transform RoomSpawner(Transform roomPrefab, Vector3 spawnLocation)
     {
+        // Instantiate and spawn a room based on the provided roomPrefab and spawnLocation
         Transform roomTransform = Instantiate(roomPrefab, spawnLocation, Quaternion.identity);
         return roomTransform;
     }
 
-    // define the room lists for different difficulty levels in the Inspector.
-    [SerializeField] private List<Transform> roomListHard;
+    private void SpawnBossRoom()
+    {
+        if (bossRoomPrefab != null)
+        {
+            // Spawn a boss room using the provided bossRoomPrefab
+            Transform bossRoomTransform = Instantiate(bossRoomPrefab, lastRoomExit, Quaternion.identity);
+            lastRoomExit = bossRoomTransform.Find("RoomExit").position;
+        }
+        else
+        {
+            Debug.LogError("Boss room prefab is not assigned.");
+        }
+    }
+
+    private void SpawnFinalRoom()
+    {
+        if (finalRoomList.Count > 0)
+        {
+            // Select a final room prefab from the finalRoomList
+            int randomIndex = Random.Range(0, finalRoomList.Count);
+            Transform finalRoomPrefab = finalRoomList[randomIndex];
+
+            if (finalRoomPrefab != null)
+            {
+                // Spawn the selected final room using the finalRoomPrefab
+                Transform finalRoomTransform = Instantiate(finalRoomPrefab, lastRoomExit, Quaternion.identity);
+                lastRoomExit = finalRoomTransform.Find("RoomExit").position;
+            }
+            else
+            {
+                Debug.LogError("Final room prefab is not assigned.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No final room prefabs assigned to finalRoomList.");
+        }
+    }
+
+    [SerializeField] private List<Transform> roomListHard; // List of hard rooms
 }
