f6024d7f341a295c7ca34cd2a596a5d443d83334
SebChamp360 <Sebastien2.Llewellyn@live.uwe.ac.uk>
2023-11-14 19:42:33 +0000

    // Health Prefabs are all named correctly
    // Changed the name of all the prefabs and the what is part of them
    // Made sure that the images are correct in the prefabs
    // Deleted the highscore part of the game
    // Added // to the scripts so it more clear what each part does

--- Unity_CGD_2023/Assets/Scripts/UI/Health.cs
+++ Unity_CGD_2023/Assets/Scripts/UI/Health.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
+    // Sets the variables 
     public Image healthBar;
     public float healthAmount = 100f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        // If a key is pressed
+        // The health bar decreases
         if (Input.GetKeyDown(KeyCode.Z))
         {
             TakeDamage(20);
         }
+        // If a key is pressed
+        // The health bar increases
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Heal(5);
@@ -31,9 +30,11 @@ public class Health : MonoBehaviour
         healthAmount -= damage;
         healthBar.fillAmount = healthAmount / 100f;
     }
+
     public void Heal(float healingAmount)
     {
         healthAmount += healingAmount;
+        // Sets the min and maz value to 0 and 100
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
 
         healthBar.fillAmount = healthAmount / 100f;
