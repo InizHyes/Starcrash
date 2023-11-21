f20d9dcdd79367650b35483031b7d4565571bcf4
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2023-11-21 01:32:18 +0000

    Tractor Enemy 1.1
    TbeamBehaviour script:
    
    - Added control over player rigidbodies, to freeze their postions upon contact with the beam, and to unfreeze once player is no longer in beam.
    
    Tractor prefab:
    
    - Changed boxtrigger to enable istrigger for script above.
    
    Current issues:
    
    - Tractor class will need to stop velocity when player is in beam
    
    Unable to turn on and off beam ability during runtime within state management.

--- Unity_CGD_2023/Assets/Scripts/Enemies Scripts/Enemy Attributes/TbeamBehaviour.cs
+++ Unity_CGD_2023/Assets/Scripts/Enemies Scripts/Enemy Attributes/TbeamBehaviour.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class TbeamBehaviour : MonoBehaviour
 {
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
         if (collision.gameObject.tag == "Player")
         {
+            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            playerRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
             // Disable movement but not rotation,
             // Players should be able to fight back and escape by themsveles or help from other free players, but with diffculty
 
             Debug.Log("Player is in tractor beam");
         }
     }
+
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        playerRigidbody.constraints = RigidbodyConstraints2D.None;
+
+        Debug.Log("Player is not in tractor beam");
+    }
+
 }
