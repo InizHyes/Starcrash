b9d1b064da80fcf41c8d9f9e77c1a3936991ed3b
Hadley <hadley2.wilson@live.uwe.ac.uk>
2023-11-21 01:37:12 +0000

    Game Systems
    +pressure plate adjustments to allow more pressure plates to be done at once
    + A timer for rooms that must be completed in a specific time (can be changed in inspector)

--- Unity_CGD_2023/Assets/Scripts/Level Scripts/Pressure plate.cs
+++ Unity_CGD_2023/Assets/Scripts/Level Scripts/Pressure plate.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    public TargetObject targetObject; // Drag and drop the TargetObject script here in the Unity Editor
+    public TargetObject targetObject; // Drag and drop the specific TargetObject script here in the Unity Editor
 
     public float timerDuration = 5f; // Adjust the duration in the Inspector
     private float timer = 0f;
+
+    public static int activePressurePlates = 0; // Static variable to track the number of active pressure plates
+    public int requiredPressurePlates = 2; // Adjust the required number in the Inspector
 
     void Update()
     {
@@ -20,26 +23,54 @@ public class PressurePlate : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerEnter(Collider other)
     {
         // Check if the player is standing on the pressure plate
-        if (collider.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("Player touched plate");
             // Activate the timer
             timer = timerDuration;
 
-            // Call the ReactToPressurePlate function on the TargetObject
+            // Increment the number of active pressure plates
+            activePressurePlates++;
+
+            // Call the ReactToPressurePlate function on the specific TargetObject
             targetObject.ReactToPressurePlate();
+
+            // Check if the required number of pressure plates is reached
+            if (activePressurePlates >= requiredPressurePlates)
+            {
+                // Trigger the behavior when the required number is reached
+                ActivateRequiredPressurePlates();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Check if the player leaves the pressure plate
+        if (other.CompareTag("Player"))
+        {
+            // Decrement the number of active pressure plates
+            activePressurePlates--;
+
+            // Ensure the number of active pressure plates doesn't go below zero
+            activePressurePlates = Mathf.Max(0, activePressurePlates);
         }
     }
 
     void ReactToTimer()
     {
-      
+        // Do something when the timer reaches zero
         Debug.Log("Timer reached zero! Reacting to timer.");
 
-        // call a method on TargetObject
+        // Call a method on the specific TargetObject or perform other actions here
         targetObject.ReactToTimer();
+    }
+
+    void ActivateRequiredPressurePlates()
+    {
+        // Do something when the required number of pressure plates is reached
+        Debug.Log("Required number of pressure plates reached! Activating something.");
     }
 }
