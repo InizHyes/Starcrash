7a22a8fc2db9da56f82c334f2a1675054d5cbcd5
James <72135268+JamesACutts@users.noreply.github.com>
2023-11-22 02:21:29 +0000

    Health System v1
    Added Health to Player
    
    (Need damage number from enemy to be able to damage the player)

---
+++ Unity_CGD_2023/Assets/Scripts/Player Scripts/CharacterStats.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [Header("Max Stats")]
+    [SerializeField] public float maxHealth;
+
+    [Header("Stats")]
+    [SerializeField] public float health;
+
+    [Header("Stats Flags")]
+    [SerializeField] protected bool isDead;
+
+    private void Start()
+    {
+        InitVariables();
+    }
+    public virtual void CheckStats()
+    {
+        if(health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+        if(health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public virtual void Die()
+    {
+        isDead = true;
+    }
+
+    public virtual void SetStatsTo(float healthSetTo)
+    {
+        health = healthSetTo;
+        CheckStats();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        float healthAfterDamage = health - damage;
+        SetStatsTo(healthAfterDamage);
+    }
+
+    public void Heal(float heal)
+    {
+        float healthAfterHeal = health + heal;
+        SetStatsTo(healthAfterHeal);
+    }
+    public virtual void InitVariables()
+    {
+        maxHealth = 100;
+        SetStatsTo(maxHealth);
+        isDead = false;
+    }
+}
