bb68288295da5af6ec4d79f99d1c26a7c462a2e1
Oliver Chalk <oliver2.chalk@live.uwe.ac.uk>
2023-11-27 11:41:04 +0000

    Boss Level V0.2
    +BossTubeScript now inherits from TubeLogic

--- Unity_CGD_2023/Assets/Boss Tube Script.cs
+++ Unity_CGD_2023/Assets/Boss Tube Script.cs
@@ -3,74 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BossTubeLogic : MonoBehaviour
+public class BossTubeLogic : TubeLogic
 {
-    #region [All References]
-    [Header("Tube Prefabs")]
-
-    public GameObject UnBrokenTube;
-    public GameObject BrokenTube;
-
-
-    // Bool
-    public bool IsBroken;
-
-    [Header("Choose what prefabs to spawn")]
-    public List<GameObject> SpawnEntity;
-
+    [Header("Boss Tube Specific")]
     [Header("Color Pressure Plate")]
     public PressurePlateScript connectedPressurePlate;
 
     public GlobalIntegerScript globalIntegerScript;
 
-    #endregion
-
-    #region [Start and OnCollision]
-
-    public void Start()
-    {
-        IsBroken = false;
-    }
-
-
-
-    public void OnCollisionEnter2D(Collision2D collision)
+    public override void OnCollisionEnter2D(Collision2D collision)
     {
         if (connectedPressurePlate != null && connectedPressurePlate.isFirstPressurePlateActivated && IsBroken == false)
         {
             // Check if the collision is with the object you want to trigger the destruction
             if (collision.gameObject.CompareTag("Bullet"))
             {
-                Debug.Log("hit");
-            IsBroken = true;
-            entitySpawn();
+                IsBroken = true;
+                entitySpawn();
             }
         }
     }
-
-    #endregion
-
-
-    public void entitySpawn()
-    {
-        if (SpawnEntity == null)
-        {
-            Debug.Log("Nothing was spawned");
-        }
-
-        if (SpawnEntity != null)
-        {
-            // Get enemy list, randomise what enemy should be spawned
-            GameObject selectedEntity = SpawnEntity[Random.Range(0, SpawnEntity.Count)];
-
-
-            Instantiate(BrokenTube, this.transform.position, Quaternion.identity);
-
-            Instantiate(selectedEntity, this.transform.position, Quaternion.identity);
-            Debug.Log("Random entity was spawned");
-
-            Destroy(UnBrokenTube);
-        }
-    }
-
 }
