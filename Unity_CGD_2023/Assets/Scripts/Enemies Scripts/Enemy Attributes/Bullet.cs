37ac93de7bf3f63f65387190bf7eb13c210e1968
Reece Dyke <reece2.dyke@live.uwe.ac.uk>
2023-12-22 14:37:21 +0000

    Straight Shooter Sprite and Enemy Bullet Projectiles Changed
    Bullets also now collide with player and causes damage to player, but no death state as player needs to be downed

--- Unity_CGD_2023/Assets/Scripts/Enemies Scripts/Enemy Attributes/Bullet.cs
+++ Unity_CGD_2023/Assets/Scripts/Enemies Scripts/Enemy Attributes/Bullet.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+    [Header("BulletDamage")]
+    public int damage = 10;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             print("Hit");
+
+            // Get the PlayerHealth component from the colliding GameObject
+            PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
+            // Check if the PlayerHealth component is not null
+            if (playerStats != null)
+            {
+                // Deal damage to the player
+                playerStats.TakeDamage(damage);
+            }
+
 
             // Destroy the bullet on impact with the player
             Destroy(gameObject);
