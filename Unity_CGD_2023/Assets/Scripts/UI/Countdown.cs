b1f575c165c30cc835ee906f19091ac340a73972
SebChamp360 <Sebastien2.Llewellyn@live.uwe.ac.uk>
2024-01-31 07:47:41 +0000

    Timer v1.2 and countdown timer v1.2
    // The timer has been reverted back to a sqaure slider from the image because it looks better for the game.
    // The countdown timer now shows a countdown from 3 to 0 then a Go text will appear on screen.
    // There are different things that can be changed for the beta such as the colour, position and names of objects

--- Unity_CGD_2023/Assets/Scripts/UI/Countdown.cs
+++ Unity_CGD_2023/Assets/Scripts/UI/Countdown.cs
@@ -6,61 +6,28 @@ using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
-    // Variables
-    public Image uiFill;
+    [SerializeField] TextMeshProUGUI timer_Text;
+    [SerializeField] TextMeshProUGUI gameover;
+    [SerializeField] float remainingTime;
 
-    public Image uiBackground;
-
-    public int totalTime;
-
-    private int timeRemaining;
-
-    public TextMeshProUGUI text;
-
-    public TextMeshProUGUI go_text;
-
-    // Start is called before the first frame update
     private void Start()
     {
-        // Sets the start time
-        begin(totalTime);
-        go_text.enabled = false;
+        gameover.enabled = false;
     }
-
-    private void begin(int Second)
-    {
-        // Starts the timer
-        timeRemaining = Second;
-        StartCoroutine(UpdateTimer());
-    }
-
-    private IEnumerator UpdateTimer()
-    {
-        // Updates the timer every second
-        while (timeRemaining >= 0)
-        {
-            uiFill.fillAmount = Mathf.InverseLerp(0, totalTime, timeRemaining);
-            timeRemaining--;
-            yield return new WaitForSeconds(1f);
-        }
-        OnEnd();
-    }
-
-    private void OnEnd()
-    {
-        // Prints out text to say timer is ended
-        print("Go");
-        text.enabled = false;
-        go_text.enabled = true;
-        uiFill.enabled = false;
-        uiBackground.enabled = false;
-    }
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (remainingTime > 0)
         {
-            go_text.enabled = false;
+            remainingTime -= Time.deltaTime;
         }
+        else if(remainingTime < 0)
+        {
+            remainingTime = 0;
+            gameover.enabled = true;
+            timer_Text.enabled = false;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timer_Text.text = string.Format("{0:00}:{1:00}",minutes, seconds);
     }
 }
