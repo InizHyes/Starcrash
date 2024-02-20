0cbdd34fe303b405d105e81e05aabfacd64a2b17
cameronallen0 <cameronjallen3@gmail.com>
2024-02-20 10:47:44 +0000

    camera push

---
+++ Unity_CGD_2023/Assets/Scripts/Player Scripts/NewCameraScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraScript : MonoBehaviour
+{
+    public Transform targetPosition;
+    public float cameraSpeed = 2.5f;
+    public string targetTag = "CameraTarget";
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        FindNewTarget();
+    }
+
+    void Update()
+    {
+        FindNewTarget();
+    }
+
+    Transform FindPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            return player.transform;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    void FindNewTarget()
+    {
+        GameObject[] emptyObjects = GameObject.FindGameObjectsWithTag(targetTag);
+        if (emptyObjects.Length == 0)
+        {
+            Debug.LogWarning("NO TARGETS FOR CAMERA");
+            return;
+        }
+
+        Transform playerTransform = FindPlayer();
+        if (playerTransform == null)
+        {
+            Debug.LogWarning("NO PLAYER FOUND");
+            return;
+        }
+
+        GameObject nearestObject = null;
+        float shortestDistance = float.MaxValue;
+
+        foreach (GameObject emptyObject in emptyObjects)
+        {
+            float distance = Vector2.Distance(playerTransform.position, emptyObject.transform.position);
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                nearestObject = emptyObject;
+            }
+        }
+        if (nearestObject != null)
+        {
+            Debug.Log("Next room found");
+            targetPosition = nearestObject.transform;
+            MoveCameraToTarget();
+        }
+    }
+
+    void MoveCameraToTarget()
+    {
+        Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition.position, cameraSpeed * Time.deltaTime);
+        transform.position = new Vector3(newPosition.x, newPosition.y, newPosition.z);
+    }
+}
