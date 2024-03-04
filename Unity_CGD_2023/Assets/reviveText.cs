c0a80d68d2b2f990ece8e6ab680adea98b189344
GitExcel <sean.harding@live.co.uk>
2024-03-04 23:43:23 +0000

    not working

--- Unity_CGD_2023/Assets/reviveText.cs
+++ Unity_CGD_2023/Assets/reviveText.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class ReviveText : MonoBehaviour
 {
 
-    public Transform child;
+    public GameObject child;
 
     void Update()
     {
         ///child.transform.rotation = Quaternion.Euler(0.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
         ///child.transform.position = new Vector2(this.transform.position.x + 0.5f, this.transform.position.y + 0.3f);
-        child.transform.position = transform.localPosition;
+        child.transform.position = transform.position;
     }
 
 }
