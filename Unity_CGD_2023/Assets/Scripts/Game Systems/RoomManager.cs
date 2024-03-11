ac92b1202c5c851ff5e13bf41c551b181cda1391
Reece Dyke <reece2.dyke@live.uwe.ac.uk>
2024-03-11 21:38:28 +0000

    Increased enemy health. Added screen ui for completed rooms

--- Unity_CGD_2023/Assets/Scripts/Game Systems/RoomManager.cs
+++ Unity_CGD_2023/Assets/Scripts/Game Systems/RoomManager.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RoomManager : MonoBehaviour
 {
     public int numberOfRoomsCompleted = 0;
     public int numberOfRoomsTotal = 9;
     public GameObject bossToSpawn;
-    
+
+    public TMP_Text roomsCompletedText; // Reference to the Text UI element
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateRoomsCompletedText();
+
         if (numberOfRoomsCompleted == 8)
         {
             bossToSpawn.SetActive(true);
@@ -25,5 +29,11 @@ public class RoomManager : MonoBehaviour
     public void SpawnBoss()
     {
         bossToSpawn.SetActive(true);
+    }
+
+    // Method to update the text value to reflect the current value of numberOfRoomsCompleted
+    void UpdateRoomsCompletedText()
+    {
+        roomsCompletedText.text = "Rooms Completed: " + numberOfRoomsCompleted.ToString() + " / " + numberOfRoomsTotal.ToString();
     }
 }
