15939c5dd7b1a8336c3193b0470fdbe4c33410c1
GitExcel <sean.harding@live.co.uk>
2024-04-26 00:21:18 +0100

    v1.1
    -Bug fix: players no longer revive each other

--- Unity_CGD_2023/Assets/Scripts/Player Scripts/reviveBox.cs
+++ Unity_CGD_2023/Assets/Scripts/Player Scripts/reviveBox.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class reviveBox : MonoBehaviour
 {
     public bool reviving;
+    private List<GameObject> playersin = new List<GameObject>();
     // Start is called before the first frame update
 
-    // Update is called once per frame
+   
 
-
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        
+        if (playersin.Count > 0)
+        {
+            foreach (var player in playersin)
+            {
+                if (player.GetComponent<Down>().downed == false)
+                {
+                    reviving = true ;
+                    break;
+                }
+                else
+                {
+                    reviving = false;
+                }
+            }
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.gameObject.CompareTag("Player"))
         {
-            print("colldingplayer");
-            print(collision.gameObject.name);
-            reviving = true;
-            ///print(reviving);
+            playersin.Add(collision.gameObject);
         }
-
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            playersin.Remove(collision.gameObject);
             reviving = false;
             ///print(reviving);
         }
